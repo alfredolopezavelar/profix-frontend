9b2e2b971c84eabf382a0c87886278ed5521828a
Alfredo López <alfredolopezavelar1@gmail.com>
2025-04-21 18:31:24 -0600

    Wrapped the app with the AuthProvider os useAuth

--- profix-frontend/src/main.tsx
+++ profix-frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/muiTheme.ts";
+import { AuthProvider } from "./hooks/useAuth.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 );
